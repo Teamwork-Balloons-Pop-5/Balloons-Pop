ee1c8574ab806db4d618f15bf373a9c1f48be233
dimitarsd <dsd363978@yahoo.com>
2015-09-26 21:41:45 +0300

    Removed the logic from the main method into two new methods inside the StartPoint class

--- BalloonsPopTeamwork/src/BalloonsPop/Console/StartPoint.cs
+++ BalloonsPopTeamwork/src/BalloonsPop/Console/StartPoint.cs
@@ -8,6 +8,124 @@
 
     public class StartPoint
     {
+        public static void ReadCommands(string temp, byte[,] matrix, int userMoves, string[,] topFive)
+        {
+            while (temp != "EXIT")
+            {
+                Console.WriteLine(GlobalGameMessages.AskingToEnterRowAndColumnMessage);
+                temp = Console.ReadLine();
+                temp = temp.ToUpper().Trim();
+
+                switch (temp)
+                {
+                    case "RESTART":
+                        PrintingMatrixOnConsole(matrix);
+                        userMoves = 0;
+                        break;
+
+                    case "TOP":
+                        SortAndPrintChartFive(topFive);
+                        break;
+
+                    default:
+                        if ((temp.Length == 3) && (temp[0] >= '0' && temp[0] <= '9') && (temp[2] >= '0' && temp[2] <= '9') && (temp[1] == ' ' || temp[1] == '.' || temp[1] == ','))
+                        {
+                            int userRow, userColumn;
+                            userRow = int.Parse(temp[0].ToString());
+
+                            if (userRow > 4)
+                            {
+                                Console.WriteLine(GlobalGameMessages.WrongInputMessage);
+                                continue;
+                            }
+
+                            userColumn = int.Parse(temp[2].ToString());
+
+                            if (Change(matrix, userRow, userColumn))
+                            {
+                                Console.WriteLine(GlobalGameMessages.TryingToPopMissingBalloonMessage);
+                                continue;
+                            }
+
+                            userMoves++;
+
+                            if (CheckIfWinner(matrix))
+                            {
+                                Console.WriteLine(GlobalGameMessages.InTopFiveWinningMessage, userMoves);
+                                if (topFive.SignIfSkilled(userMoves))
+                                {
+                                    SortAndPrintChartFive(topFive);
+                                }
+                                else
+                                {
+                                    Console.WriteLine(GlobalGameMessages.NotInTopFiveWinningMessage);
+                                }
+
+                                matrix = GenerateBalloons(5, 10);
+                                userMoves = 0;
+                            }
+
+                            PrintingMatrixOnConsole(matrix);
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine(GlobalGameMessages.WrongInputMessage);
+                            break;
+                        }
+                }
+            }
+
+            return;
+        }
+
+        public static void PrintingMatrixOnConsole(byte[,] matrix)
+        {
+            Console.Write("    ");
+
+            for (byte column = 0; column < matrix.GetLongLength(1); column++)
+            {
+                Console.Write(column + " ");
+            }
+
+            Console.Write("\n   ");
+
+            for (byte column = 0; column < (matrix.GetLongLength(1) * 2) + 1; column++)
+            {
+                Console.Write("-");
+            }
+
+            Console.WriteLine();
+
+            for (byte i = 0; i < matrix.GetLongLength(0); i++)
+            {
+                Console.Write(i + " | ");
+
+                for (byte j = 0; j < matrix.GetLongLength(1); j++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        Console.Write("  ");
+                        continue;
+                    }
+
+                    Console.Write(matrix[i, j] + " ");
+                }
+
+                Console.Write("| ");
+                Console.WriteLine();
+            }
+
+            Console.Write("   ");
+
+            for (byte column = 0; column < (matrix.GetLongLength(1) * 2) + 1; column++)
+            {
+                Console.Write("-");
+            }
+
+            Console.WriteLine();
+        }
+
         public static byte[,] GenerateBalloons(byte rows, byte columns)
         {
             byte[,] balloonsMatrix = new byte[rows, columns];
@@ -201,197 +319,14 @@
             string[,] topFive = new string[5, 2];
             byte[,] matrix = GenerateBalloons(5, 10);
 
-            Console.Write("    ");
+            PrintingMatrixOnConsole(matrix);
 
-            for (byte column = 0; column < matrix.GetLongLength(1); column++)
-            {
-                Console.Write(column + " ");
-            }
-
-            Console.Write("\n   ");
-
-            for (byte column = 0; column < (matrix.GetLongLength(1) * 2) + 1; column++)
-            {
-                Console.Write("-");
-            }
-
-            Console.WriteLine();
-
-            for (byte i = 0; i < matrix.GetLongLength(0); i++)
-            {
-                Console.Write(i + " | ");
-
-                for (byte j = 0; j < matrix.GetLongLength(1); j++)
-                {
-                    if (matrix[i, j] == 0)
-                    {
-                        Console.Write("  ");
-                        continue;
-                    }
-
-                    Console.Write(matrix[i, j] + " ");
-                }
-
-                Console.Write("| ");
-                Console.WriteLine();
-            }
-
-            Console.Write("   ");
-
-            for (byte column = 0; column < (matrix.GetLongLength(1) * 2) + 1; column++)
-            {
-                Console.Write("-");
-            }
-
-            Console.WriteLine();
             string temp = null;
             int userMoves = 0;
 
-            while (temp != "EXIT")
-            {
-                Console.WriteLine(GlobalGameMessages.AskingToEnterRowAndColumnMessage);
-                temp = Console.ReadLine();
-                temp = temp.ToUpper().Trim();
+            ReadCommands(temp, matrix, userMoves, topFive);
 
-                switch (temp)
-                {
-                    case "RESTART":
-                        matrix = GenerateBalloons(5, 10);
-                        Console.Write("    ");
-                        for (byte column = 0; column < matrix.GetLongLength(1); column++)
-                        {
-                            Console.Write(column + " ");
-                        }
-
-                        Console.Write("\n   ");
-                        for (byte column = 0; column < (matrix.GetLongLength(1) * 2) + 1; column++)
-                        {
-                            Console.Write("-");
-                        }
-
-                        Console.WriteLine();
-
-                        for (byte i = 0; i < matrix.GetLongLength(0); i++)
-                        {
-                            Console.Write(i + " | ");
-                            for (byte j = 0; j < matrix.GetLongLength(1); j++)
-                            {
-                                if (matrix[i, j] == 0)
-                                {
-                                    Console.Write("  ");
-                                    continue;
-                                }
-
-                                Console.Write(matrix[i, j] + " ");
-                            }
-
-                            Console.Write("| ");
-                            Console.WriteLine();
-                        }
-
-                        Console.Write("   ");
-                        for (byte column = 0; column < (matrix.GetLongLength(1) * 2) + 1; column++)
-                        {
-                            Console.Write("-");
-                        }
-
-                        Console.WriteLine();
-                        userMoves = 0;
-                        break;
-
-                    case "TOP":
-                        SortAndPrintChartFive(topFive);
-                        break;
-
-                    default:
-                        if ((temp.Length == 3) && (temp[0] >= '0' && temp[0] <= '9') && (temp[2] >= '0' && temp[2] <= '9') && (temp[1] == ' ' || temp[1] == '.' || temp[1] == ','))
-                        {
-                            int userRow, userColumn;
-                            userRow = int.Parse(temp[0].ToString());
-
-                            if (userRow > 4)
-                            {
-                                Console.WriteLine(GlobalGameMessages.WrongInputMessage);
-                                continue;
-                            }
-
-                            userColumn = int.Parse(temp[2].ToString());
-
-                            if (Change(matrix, userRow, userColumn))
-                            {
-                                Console.WriteLine(GlobalGameMessages.TryingToPopMissingBalloonMessage);
-                                continue;
-                            }
-
-                            userMoves++;
-
-                            if (CheckIfWinner(matrix))
-                            {
-                                Console.WriteLine(GlobalGameMessages.InTopFiveWinningMessage, userMoves);
-                                if (topFive.SignIfSkilled(userMoves))
-                                {
-                                    SortAndPrintChartFive(topFive);
-                                }
-                                else
-                                {
-                                    Console.WriteLine(GlobalGameMessages.NotInTopFiveWinningMessage);
-                                }
-
-                                matrix = GenerateBalloons(5, 10);
-                                userMoves = 0;
-                            }
-
-                            Console.Write("    ");
-                            for (byte column = 0; column < matrix.GetLongLength(1); column++)
-                            {
-                                Console.Write(column + " ");
-                            }
-
-                            Console.Write("\n   ");
-                            for (byte column = 0; column < (matrix.GetLongLength(1) * 2) + 1; column++)
-                            {
-                                Console.Write("-");
-                            }
-
-                            Console.WriteLine();
-
-                            for (byte i = 0; i < matrix.GetLongLength(0); i++)
-                            {
-                                Console.Write(i + " | ");
-                                for (byte j = 0; j < matrix.GetLongLength(1); j++)
-                                {
-                                    if (matrix[i, j] == 0)
-                                    {
-                                        Console.Write("  ");
-                                        continue;
-                                    }
-
-                                    Console.Write(matrix[i, j] + " ");
-                                }
-
-                                Console.Write("| ");
-                                Console.WriteLine();
-                            }
-
-                            Console.Write("   ");
-
-                            for (byte column = 0; column < (matrix.GetLongLength(1) * 2) + 1; column++)
-                            {
-                                Console.Write("-");
-                            }
-
-                            Console.WriteLine();
-                            break;
-                        }
-                        else
-                        {
-                            Console.WriteLine(GlobalGameMessages.WrongInputMessage);
-                            break;
-                        }
-                }
-            }
-
-            Console.WriteLine("Good Bye! ");
+            Console.WriteLine(GlobalGameMessages.ExitGameMessage);
         }
     }
 }
