a249378471645f8eb74fb0f41b36a6f8856b2037
dimitarsd <dsd363978@yahoo.com>
2015-09-26 20:30:31 +0300

    Added constants for printing matrix, game messages and top five chart

--- BalloonsPopTeamwork/src/BalloonsPop/Common/Constants/GlobalGameMessages.cs
+++ BalloonsPopTeamwork/src/BalloonsPop/Common/Constants/GlobalGameMessages.cs
@@ -2,6 +2,11 @@
 {
     public class GlobalGameMessages
     {
-        // Add here all game messages
+        public const string AskingToEnterRowAndColumnMessage = "Enter a row and column:";
+        public const string WrongInputMessage = "Wrong input! Try Again!";
+        public const string TryingToPopMissingBalloonMessage = "Cannot pop missing balloon!";
+        public const string InTopFiveWinningMessage = "Congratulations ! You completed it in {0} moves.";
+        public const string NotInTopFiveWinningMessage = "I am sorry you are not skillful enough for TopFive chart!";
+        public const string ExitGameMessage = "Good Bye!";
     }
 }
---
+++ BalloonsPopTeamwork/src/BalloonsPop/Common/Constants/PrintingMatrixGraphicalInterface.cs
@@ -0,0 +1,9 @@
+﻿namespace BalloonsPop.Common.Constants
+{
+    public class PrintingMatrixGraphicalInterface
+    {
+        public const string MatrixHorizontalBorderSymbol = "-";
+        public const string MatrixVerticalLeftBorderSymbol = "| ";
+        public const string MatrixVerticalRightBorderSymbol = " | ";
+    }
+}
