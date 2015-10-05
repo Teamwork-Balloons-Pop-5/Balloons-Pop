d98b4ff582eba4d751dc2c1c64247c761ca4edc1
alexizvely <alexizvely@gmail.com>
2015-10-05 19:41:41 +0300

    Game messages are rephrased in GlobalGameMessages.cs

--- BalloonsPopTeamwork/src/BalloonsPop/Common/Constants/GlobalGameMessages.cs
+++ BalloonsPopTeamwork/src/BalloonsPop/Common/Constants/GlobalGameMessages.cs
@@ -3,12 +3,12 @@
     public class GlobalGameMessages
     {
         public const string WelcomeMessage = "Welcome to Balloons Pop Game";
-        public const string AskingToEnterRowAndColumnMessage = "Enter a row and column:";
-        public const string WrongInputMessage = "Wrong input! Try Again!";
-        public const string TryingToPopMissingBalloonMessage = "Cannot pop missing balloon!";
-        public const string InTopFiveWinningMessage = "Congratulations ! You completed it in {0} moves.";
-        public const string NotInTopFiveWinningMessage = "I am sorry you are not skillful enough for TopFive chart!";
+        public const string AskingToEnterRowAndColumnMessage = "Please enter a row and column: ";
+        public const string WrongInputMessage = "You have inserted incorrect input - please retry.";
+        public const string TryingToPopMissingBalloonMessage = "You cannot pop a missing balloon - please retry.";
+        public const string InTopFiveWinningMessage = "Congratulations ! You completed the game in {0} moves.";
+        public const string NotInTopFiveWinningMessage = "You need to complete the game in less moves to enter the Top Chart. Better luck next time!";
         public const string ExitGameMessage = "Good Bye!";
-        public const string AskingForUserNameMessage = "Please enter your name for the top scoreboard: ";
+        public const string AskingForUserNameMessage = "Please enter your name for the Top Chart: ";
     }
 }
