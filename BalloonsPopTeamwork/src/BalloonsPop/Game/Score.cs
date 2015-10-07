b74f06a949eedad67745394312446a7718ac37b6
alexizvely <alexizvely@gmail.com>
2015-10-07 16:43:37 +0300

    adjusted validator in Validator.cs adjusted and added exceptions in Exception.cs implemented validators in the setters added Score class in Score.cs added HighScore.txt - adding the new score, sorting the highscores and saving only the top N highscores needed in the file

---
+++ BalloonsPopTeamwork/src/BalloonsPop/Game/Score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalloonsPop.Game
+{
+    public class Score
+    {
+
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public Score(string name, int points)
+        {
+            this.Name = name;
+            this.Points = points;
+        }
+        public string ToString()
+        {
+            return this.Name + "-" + this.Points.ToString();
+        }
+
+    }
+}
