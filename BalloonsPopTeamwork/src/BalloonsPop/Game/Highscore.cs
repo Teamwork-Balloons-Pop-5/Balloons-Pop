907febd1726be382fdc520011e3cb9b22ba031d7
alexizvely <alexizvely@gmail.com>
2015-10-12 07:55:00 +0300

    more documentation

--- BalloonsPopTeamwork/src/BalloonsPop/Game/Highscore.cs
+++ BalloonsPopTeamwork/src/BalloonsPop/Game/Highscore.cs
@@ -1,24 +1,50 @@
-﻿namespace BalloonsPop.Game
+﻿// <copyright  file="Highscore.cs" company="Balloons-Pop-5">
+// All rights reserved.
+// </copyright>
+// <author>DimitarSD, alexizvely, fr0wsTyl</author>
+
+namespace BalloonsPop.Game
 {
     using System;
     using BalloonsPop.Common;
     using BalloonsPop.Common.Exceptions;
 
+    /// <summary>
+    /// creates instance of the scoreboard
+    /// </summary>
     public class Highscore : IComparable<Highscore>
     {
+        /// <summary>
+        /// This field holds the points (spent moves from the user)
+        /// </summary>
         private int value;
+
+        /// <summary>
+        /// This field holds the username
+        /// </summary>
         private string name;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Highscore" /> class.
+        /// </summary>
         public Highscore()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Highscore" /> class.
+        /// </summary>
+        /// /// <param name="name">string that holds the username</param>
+        /// <param name="value">An integer that holds the points (spent moves from the user)</param>
         public Highscore(string name, int value)
         {
             this.Value = value;
             this.Name = name;
         }
 
+        /// <summary>
+        /// Gets or sets the points;
+        /// </summary>
         public int Value
         {
             get
@@ -37,6 +63,9 @@
             }
         }
 
+        /// <summary>
+        /// Gets or sets the username;
+        /// </summary>
         public string Name
         {
             get
@@ -55,11 +84,18 @@
             }
         }
 
+        /// <summary>
+        /// Compares two highscores
+        /// </summary>
+        /// <param name="other">takes highscore to compare to</param>
         public int CompareTo(Highscore other)
         {
             return this.Value.CompareTo(other.Value);
         }
 
+        /// <summary>
+        /// stringifies a highscore
+        /// </summary>
         public override string ToString()
         {
             return this.Name + "-" + this.Value.ToString();
--- BalloonsPopTeamwork/src/BalloonsPop/Game/Matrix.cs
+++ BalloonsPopTeamwork/src/BalloonsPop/Game/Matrix.cs
@@ -1,9 +1,24 @@
-﻿namespace BalloonsPop.Game
+﻿// <copyright  file="Matrix.cs" company="Balloons-Pop-5">
+// All rights reserved.
+// </copyright>
+// <author>DimitarSD, alexizvely, fr0wsTyl</author>
+
+namespace BalloonsPop.Game
 {
     using BalloonsPop.Console.ConsoleUI.Playfield;
 
+    /// <summary>
+    /// creates instance of the playfield that gets modified
+    /// </summary>
     public class Matrix
     {
+        /// <summary>
+        /// checks wether the matrix needs to be modified
+        /// </summary>
+        /// <param name="matrixToModify">object form type playfield that represents the matrix that hold sthe balloons</param>
+        /// <param name="row">matrix size</param>
+        /// <param name="column">matrix size</param>
+        /// <returns>bool value</returns>
         public static bool ChangeMatrix(Playfield matrixToModify, int row, int column)
         {
             if (matrixToModify.Field[row, column] == "0")
