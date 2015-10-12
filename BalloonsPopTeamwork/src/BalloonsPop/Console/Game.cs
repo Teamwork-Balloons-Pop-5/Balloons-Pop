51a568fef70e433a2fafb5b4e6c6ce5519148b0a
alexizvely <alexizvely@gmail.com>
2015-10-12 05:06:17 +0300

    more documentation

--- BalloonsPopTeamwork/src/BalloonsPop/Console/Game.cs
+++ BalloonsPopTeamwork/src/BalloonsPop/Console/Game.cs
@@ -1,16 +1,27 @@
-﻿namespace BalloonsPop.Console
+﻿// <copyright  file="Game.cs" company="Balloons-Pop-5">
+// All rights reserved.
+// </copyright>
+// <author>DimitarSD, alexizvely, fr0wsTyl</author>
+
+namespace BalloonsPop.Console
 {
-    // Facade
+    /// <summary>
+    /// The facade for the game
+    /// </summary>
     public class Game
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Game" /> class.
+        /// </summary>
         public Game()
         {
-
         }
 
+        /// <summary>
+        /// Starts the game
+        /// </summary>
         public void Start()
         {
-
         }
     }
 }
--- BalloonsPopTeamwork/src/BalloonsPop/Console/StartPoint.cs
+++ BalloonsPopTeamwork/src/BalloonsPop/Console/StartPoint.cs
@@ -1,13 +1,23 @@
-﻿namespace BalloonsPop.Console
+﻿// <copyright  file="StartPoint.cs" company="Balloons-Pop-5">
+// All rights reserved.
+// </copyright>
+// <author>DimitarSD, alexizvely, fr0wsTyl</author>
+
+namespace BalloonsPop.Console
 {
     using System;
-
     using BalloonsPop.Common.Constants;
     using BalloonsPop.Console.ConsoleUI.Menu;
     using BalloonsPop.Engine;
 
+    /// <summary>
+    /// Balloons Pop-5 Start Point - A game is initialized with its components, ran in a loop and then terminated.
+    /// </summary>
     public class StartPoint
-    {        
+    {
+        /// <summary>
+        /// Entry point for the Balloons Pop-5 game.
+        /// </summary>
         public static void Main()
         {
             // var menu = new Menu();
